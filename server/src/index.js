94a28a1b0d29f65289c3f72f161fd6f03a55f96b
Shashank Upadhyay <shashanku346@gmail.com>
2025-11-01 11:41:22 +0530

    index.js

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,8 @@ import dbConnect from "./db/db.js"
 dotenv.config()
 
 dbConnect();
+
+console.log("hello world")
 app.listen(process.env.PORT || 8000,()=>{
     console.log("\n\nserver is running\n\n")
 })
