be6d0c7985575e5f6e7ea0c1ad6f595706d95acd
Shashank Upadhyay <shashanku346@gmail.com>
2026-01-30 18:25:14 +0530

    m

--- server/src/index.js
+++ server/src/index.js
@@ -1,15 +1,32 @@
-import dotenv from "dotenv"
-import express from "express"
-import mongoose from "mongoose"
+// import dotenv from "dotenv"
+// import express from "express"
+// import mongoose from "mongoose"
 
-import app from "./app.js"
-import dbConnect from "./db/db.js"
-dotenv.config()
+// import app from "./app.js"
+// import dbConnect from "./db/db.js"
+// dotenv.config()
 
-dbConnect();
+// dbConnect();
 
-app.listen(process.env.PORT || 8000,()=>{
-    console.log("\n\nserver is running\n\n")
-})
+// app.listen(process.env.PORT || 8000,()=>{
+//     console.log("\n\nserver is running\n\n")
+// })
+
+import dotenv from "dotenv";
+import app from "./app.js";
+import dbConnect from "./db/db.js";
+
+dotenv.config();
+
+dbConnect()
+  .then(() => {
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("DB connection failed:", err);
+  });
 
 
