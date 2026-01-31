171f86c7fff01db03d4036107018cc787b3ec934
Shashank Upadhyay <shashanku346@gmail.com>
2026-01-31 17:04:28 +0530

    final

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import { verifyJwt,verifyRole } from "./middlewares/auth.middleware.js";
 
-import { fileURLToPath } from "url";
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
